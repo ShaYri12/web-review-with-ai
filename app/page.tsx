5c1affe86c639f73b057c196cc0208e698255db6
ShaYri12 <sharjeelb60@gmail.com>
2025-01-01 04:09:26 +0500

    arrow added for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -25,11 +25,11 @@ export default function Home() {
     <div className="h-screen flex flex-col justify-between min-h-full items-center bg-[#252A2E] text-white">
       <Header />
 
-      <main className="px-4 lg:px-[24px] 3xl:py-[47px] py-[23px] my-auto">
+      <main className="px-4 lg:px-[24px] 3xl:py-[47px] py-[23px] w-full">
         <div className="3xl:max-w-[1600px] 2xl:max-w-[1280px] max-w-[1280px] mx-auto">
           <div className="">
             <div className="flex items-center space-x-4">
-              <h1 className="xl:font-[800] font-[700] text-[37px] sm:text-[40px] md:text-[48px] lg:text-[60px] xl:text-[70px] 3xl:text-[90px] 2xl:leading-[100.18px] lg:leading-[110px] md:leading-[90px] sm:leading-[80px] leading-[50px]">
+              <h1 className="xl:font-[800] font-[700] text-[37px] sm:text-[40px] md:text-[48px] lg:text-[60px] xl:text-[70px] 3xl:text-[90px] 2xl:leading-[130px] lg:leading-[110px] md:leading-[90px] sm:leading-[80px] leading-[50px]">
                 Review your Website with AI{" "}
                 <span className="inline-flex items-center">
                   <Image
@@ -58,7 +58,7 @@ export default function Home() {
 
             <div className="3xl:mt-[70px] lg:mt-[50px] md:mt-[60px] mt-[50px] flex lg:flex-row flex-col justify-between gap-8 xl:gap-[100px]">
               <div className="relative flex-1 lg:w-auto max-w-[600px]">
-                <div className="flex items-center space-x-2 md:space-x-4 lg:space-x-[20px] 3xl:space-x-[26px] 3xl:mb-[100px] lg:mb-[70px] sm:mb-[65px] mb-[40px]">
+                <div className="flex items-center space-x-2 md:space-x-4 lg:space-x-[20px] 3xl:space-x-[26px] 3xl:mb-[100px] mb-[70px]">
                   <span className="3xl:w-[50px] lg:w-[40px] md:w-[35px] w-[30px] min-w-[32px] 3xl:h-[50px] lg:h-[40px] md:h-[35px] h-[30px] rounded-full 3xl:text-[30px] lg:text-[22px] md:text-[18px] font-bold bg-cyan flex items-center justify-center">
                     1
                   </span>
@@ -69,12 +69,19 @@ export default function Home() {
                       alt="arrow-down"
                       width={150.91}
                       height={117.5}
-                      className="sm:block hidden 3xl:w-[144px] xl:w-[105px] w-[89px] 3xl:h-[117.5px] absolute 3xl:right-[-170px] xl:right-[-120px] right-[-100px] top-[15px]"
+                      className="xl:block hidden 3xl:w-[144px] xl:w-[105px] w-[89px] 3xl:h-[117.5px] absolute 3xl:right-[-170px] xl:right-[-120px] right-[-100px] top-[15px]"
+                    />
+                    <Image
+                      src="/assets/arrow-down-sm.svg"
+                      alt="arrow-down"
+                      width={109.01}
+                      height={154.16}
+                      className="xl:hidden block w-[75px] 3xl:h-[154.16px] absolute right-[-50px] top-[-14px]"
                     />
                   </span>
                 </div>
 
-                <div className="flex sm:flex-nowrap flex-wrap justify-end bg-[#5E656C] rounded-[15px] p-[4px] lg:gap-0 gap-1 3xl:w-[800px]">
+                <div className="relative flex sm:flex-nowrap flex-wrap justify-end bg-[#5E656C] rounded-[15px] p-[4px] lg:gap-0 gap-1 3xl:w-[800px]">
                   <input
                     type="text"
                     value={url}
@@ -88,10 +95,17 @@ export default function Home() {
                   >
                     Analyze
                   </button>
+                  <Image
+                    src="/assets/analyze-arrow-sm.svg"
+                    alt="arrow-up"
+                    width={277.54}
+                    height={134.5}
+                    className="lg:hidden block w-[200px] h-[134.5px] absolute right-[50px] bottom-[-120px]"
+                  />
                 </div>
               </div>
 
-              <div className="relative flex lg:flex-nowrap flex-wrap lg:items-start justify-between gap-[18px]">
+              <div className="relative flex lg:flex-nowrap flex-wrap sm:flex-row flex-col lg:items-start justify-between gap-[18px] lg:mt-0 sm:mt-[60px] mt-[20px]">
                 <div className="flex space-x-2 md:space-x-4 lg:space-x-[20px] 3xl:space-x-[26px] 3xl:mb-[80px] lg:mb-[60px] mt-[60px]">
                   <span className="lg:hidden 3xl:w-[50px] lg:w-[40px] md:w-[35px] w-[32px] min-w-[32px] 3xl:h-[50px] lg:h-[40px] md:h-[35px] h-[32px] rounded-full 3xl:text-[30px] lg:text-[22px] md:text-[18px] font-bold bg-cyan flex items-center justify-center">
                     2
@@ -120,7 +134,7 @@ export default function Home() {
                   alt="arrow-up"
                   width={414.5}
                   height={131.28}
-                  className="lg:block hidden 3xl:w-[414.5px] xl:w-[300px] w-[240px] 3xl:h-[131.28px] absolute 3xl:left-[-260px] xl:left-[-190px] left-[-120px] 3xl:top-[82%] xl:top-[73%] top-[60%] -translate-y-1/2"
+                  className="lg:block hidden 3xl:w-[414.5px] xl:w-[300px] w-[240px] 3xl:h-[131.28px] absolute 3xl:left-[-260px] xl:left-[-190px] left-[-120px] 3xl:top-[82%] xl:top-[73%] top-[70%] -translate-y-1/2"
                 />
               </div>
             </div>
