c0d51dcf4f3e00280445732fb55b5b9dfab22363
ShaYri12 <sharjeelb60@gmail.com>
2025-01-13 00:41:07 +0500

    partial report page done

--- app/partial-report/GradeBadge.tsx
+++ app/partial-report/GradeBadge.tsx
@@ -5,14 +5,14 @@ type GradeProps = {
 
 export function GradeBadge({
   grade,
-  className = "xl:w-[37.52px] xl:min-w-[37.52px] lg:w-[34px] lg:min-w-[34px] md:w-[30px] md:min-w-[30px] w-[28px] min-w-[28px] xl:h-[37.52px] lg:h-[34px] md:h-[30px] h-[28px] border-[2px] 2xl:text-[20px] xl:text-[19px] lg:text-[18px] md:text-[16px] text-[14px]",
+  className = "xl:w-[37.52px] xl:min-w-[37.52px] lg:w-[32px] lg:min-w-[32px] md:w-[30px] md:min-w-[30px] w-[28px] min-w-[28px] xl:h-[37.52px] lg:h-[32px] md:h-[30px] h-[28px] border-[2px] 2xl:text-[20px] xl:text-[19px] lg:text-[18px] md:text-[16px] text-[14px]",
 }: GradeProps) {
   const getGradeColor = (grade: string) => {
     const colors: Record<string, string> = {
       A: "bg-[#0E8B6E]",
       B: "bg-[#FFC107]",
       C: "bg-[#FF8C42]",
-      D: "bg-[#FF4D4D]",
+      F: "bg-[#FF4D4D]",
     };
 
     // Ensure `grade.charAt(0)` is a valid key or return a default value
--- app/partial-report/page.tsx
+++ app/partial-report/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import { GradeBadge } from "./GradeBadge";
-import { BiPlus } from "react-icons/bi";
+import { BiPlusCircle } from "react-icons/bi";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { MdOutlineImage } from "react-icons/md";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { LuPencil } from "react-icons/lu";
+import { FaRegHeart } from "react-icons/fa";
+import { IoMdCheckmark } from "react-icons/io";
+import { GiAlliedStar } from "react-icons/gi";
 
 export default function ReportPage() {
   return (
@@ -12,7 +18,7 @@ export default function ReportPage() {
       <main className="px-4 lg:px-[24px] 3xl:py-[47px] py-[23px] w-full relative z-[0]">
         <div className="3xl:max-w-[1600px] 2xl:max-w-[1280px] max-w-[1280px] mx-auto">
           {/* Header Card */}
-          <div className="bg-white text-gray-900 rounded-xl xl:px-[51px] lg:px-[45px] md:px-[30px] sm:px-[20px] px-4 xl:py-[42px] lg:py-[38px] md:py-[30px] py-[20px] xl:mb-[62px] lg:mb-[50px] md:mb-[40px] mb-[30px] flex justify-between items-center gap-2 md:flex-nowrap flex-wrap">
+          <div className="bg-white text-[#252A2E] rounded-[20px] xl:px-[48px] lg:px-[42px] md:px-[30px] sm:px-[20px] px-4 xl:py-[42px] lg:py-[38px] md:py-[30px] py-[20px] xl:mb-[62px] lg:mb-[50px] md:mb-[40px] mb-[30px] flex justify-between items-center gap-2 md:flex-nowrap flex-wrap">
             <div>
               <h1 className="2xl:text-[56px] xl:text-[45px] lg:text-[35px] md:text-[25px] sm:text-[20px] text-[18px] font-bold">
                 Your Custom Report
@@ -35,11 +41,11 @@ export default function ReportPage() {
             </div>
           </div>
 
-          <div className="grid lg:grid-cols-[300px,1fr] md:gap-8 gap-[100px]">
+          <div className="grid lg:grid-cols-[300px,1fr] lg:gap-8 gap-[100px]">
             {/* Left Sidebar */}
-            <div className="space-y-6">
+            <div>
               <div className="relative">
-                <div className="rounded-[10px] overflow-hidden p-4">
+                <div className="rounded-[10px] overflow-hidden">
                   <Image
                     src="/assets/laptop.png"
                     alt="Website Screenshot"
@@ -48,24 +54,24 @@ export default function ReportPage() {
                     className="w-full rounded-[10px] overflow-hidden"
                   />
                 </div>
-                <div className="absolute lg:top-[3%] top-[5%] lg:right-[8%] right-[10%]">
+                <div className="absolute lg:top-[-2%] top-[2%] lg:right-[3%] right-[7%]">
                   <GradeBadge
                     grade="B-"
-                    className="md:w-[70px] w-[55px] md:h-[70px] h-[55px] border-[4.5px] 2xl:text-[30px] xl:text-[30px] lg:text-[20px] md:text-[18px] text-[16px]"
+                    className="md:w-[70px] sm:w-[55px] w-[48px] md:h-[70px] md:h-[55px] sm:h-[55px] h-[48px] sm:border-[4.5px] border-[4px] 2xl:text-[30px] xl:text-[30px] lg:text-[20px] md:text-[18px] text-[16px]"
                   />
                 </div>
               </div>
 
-              <div className="space-y-4">
-                <h2 className="text-xl font-bold">
+              <div className="2xl:space-y-[45px] xl:space-y-[40px] lg:space-y-[35px] md:space-y-[25px] space-y-[20px] 2xl:mt-[40px] xl:mt-[35px] lg:mt-[30px] md:mt-[25px] mt-[20px]">
+                <h2 className="lg:text-[23px] md:text-[20px] text-[18px] font-bold">
                   Get your full detailed PDF report by email today!
                 </h2>
-                <button className="w-full bg-cyan text-gray-900 py-3 px-6 rounded-[10px] overflow-hidden font-semibold hover:bg-emerald-500 transition-colors">
+                <button className="w-full bg-cyan text-white 2xl:py-[18px] xl:py-[14px] lg:py-[12px] md:py-[10px] py-[9px] px-6 rounded-[10px] overflow-hidden 2xl:text-[30px] xl:text-[25px] lg:text-[22px] md:text-[20px] text-[18px] font-bold hover:bg-emerald-500 transition-colors">
                   Unlock ($4.99)
                 </button>
               </div>
 
-              <div className="relative mx-auto 3xl:w-[361px] lg:w-[240px] w-[220px] 3xl:h-[358.4px] mt-[20px]">
+              <div className="relative mx-auto 3xl:w-[361px] lg:w-[240px] w-[220px] 3xl:h-[358.4px] 2xl:mt-[70px] xl:mt-[60px] lg:mt-[50px] md:mt-[40px] mt-[30px]">
                 {/* First page - back */}
                 <div className="absolute right-0 top-8 3xl:w-[361px] lg:w-[240px] w-[220px] 3xl:h-[190px] xl:h-[180px] lg:h-[170px] h-auto mt-[20px]">
                   <Image
@@ -108,21 +114,28 @@ export default function ReportPage() {
             <div className="space-y-6">
               {/* Summary Section */}
               <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                  <div className="flex items-center gap-2">
-                    <BiPlus />
+                <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                  <div className="flex items-center 2xl:gap-[19px] xl:gap-[17px] lg:gap-[16px] md:gap-[14px] gap-2">
+                    <span>
+                      <BiPlusCircle
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
+                    </span>
 
-                    <h2 className="font-[400]">Summary</h2>
+                    <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                      Summary
+                    </h2>
                   </div>
                   <GradeBadge grade="B" />
                 </div>
                 <div className="md:px-[18px] px-4 py-[14px]">
-                  <ul className="list-disc ml-5 mb-4 text-[#252A2E]">
+                  <ul className="list-disc md:ml-6 ml-5 mb-4 text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                     <li>Our understanding of the goal of the website</li>
                     <li>High level summary of our takeaways</li>
                     <li>Grades for each category</li>
                   </ul>
-                  <p className="text-[#252A2E]">
+                  <p className="text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16] text-[15px]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -134,16 +147,21 @@ export default function ReportPage() {
               {/* Visual Appeal Section */}
               <div className="grid md:grid-cols-2 gap-6">
                 <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                    <div className="flex items-center gap-2">
-                      <BiPlus />
+                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                    <div className="flex items-center 2xl:gap-[19px] xl:gap-[17px] lg:gap-[16px] md:gap-[14px] gap-2">
+                      <MdOutlineImage
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
 
-                      <h2 className="font-[400]">Visual Appeal</h2>
+                      <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                        Visual Appeal
+                      </h2>
                     </div>
                     <GradeBadge grade="C" />
                   </div>
                   <div className="md:px-[18px] px-4 py-[14px]">
-                    <ul className="list-disc ml-5 mb-4 text-[#252A2E]">
+                    <ul className="list-disc md:ml-6 ml-5 mb-4 text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                       <li>Image quality / relevance</li>
                       <li>Use of white space</li>
                       <li>Color scheme</li>
@@ -153,16 +171,21 @@ export default function ReportPage() {
                 </div>
 
                 <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                    <div className="flex items-center gap-2">
-                      <BiPlus />
+                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                    <div className="flex items-center 2xl:gap-[19px] xl:gap-[17px] lg:gap-[16px] md:gap-[14px] gap-2">
+                      <IoPersonCircleOutline
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
 
-                      <h2 className="font-[400]">Usability</h2>
+                      <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                        Usability
+                      </h2>
                     </div>
                     <GradeBadge grade="A" />
                   </div>
                   <div className="md:px-[18px] px-4 py-[14px]">
-                    <ul className="list-disc ml-5 mb-4 text-[#252A2E]">
+                    <ul className="list-disc md:ml-6 ml-5 mb-4 text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                       <li>Ease of navigation</li>
                       <li>Mobile responsiveness</li>
                       <li>Page load speed</li>
@@ -171,16 +194,21 @@ export default function ReportPage() {
                 </div>
 
                 <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                    <div className="flex items-center gap-2">
-                      <BiPlus />
+                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                    <div className="flex items-center 2xl:gap-[19px] xl:gap-[17px] lg:gap-[16px] md:gap-[14px] gap-2">
+                      <LuPencil
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
 
-                      <h2 className="font-[400]">Content</h2>
+                      <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                        Content
+                      </h2>
                     </div>
                     <GradeBadge grade="A" />
                   </div>
                   <div className="md:px-[18px] px-4 py-[14px]">
-                    <p className="text-[#252A2E]">
+                    <p className="text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                       Sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua.
@@ -189,16 +217,21 @@ export default function ReportPage() {
                 </div>
 
                 <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                    <div className="flex items-center gap-2">
-                      <BiPlus />
+                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                    <div className="flex items-center 2xl:gap-[19px] xl:gap-[17px] lg:gap-[16px] md:gap-[14px] gap-2">
+                      <FaRegHeart
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
 
-                      <h2 className="font-[400]">Branding</h2>
+                      <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                        Branding
+                      </h2>
                     </div>
                     <GradeBadge grade="B" />
                   </div>
                   <div className="md:px-[18px] px-4 py-[14px]">
-                    <p className="text-[#252A2E]">
+                    <p className="text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                       Sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua.
@@ -207,16 +240,21 @@ export default function ReportPage() {
                 </div>
 
                 <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                    <div className="flex items-center gap-2">
-                      <BiPlus />
+                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                    <div className="flex items-center 2xl:gap-[19px] xl:gap-[17px] lg:gap-[16px] md:gap-[14px] gap-2">
+                      <IoMdCheckmark
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
 
-                      <h2 className="font-[400]">Trustworthy</h2>
+                      <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                        Trustworthy
+                      </h2>
                     </div>
                     <GradeBadge grade="C" />
                   </div>
                   <div className="md:px-[18px] px-4 py-[14px]">
-                    <p className="text-[#252A2E]">
+                    <p className="text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                       Sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua.
@@ -225,16 +263,21 @@ export default function ReportPage() {
                 </div>
 
                 <div className="bg-white text-white rounded-[10px] overflow-hidden">
-                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[6px] bg-[#5E656C]">
-                    <div className="flex items-center gap-2">
-                      <BiPlus />
+                  <div className="flex justify-between items-center md:px-[18px] px-4 py-[7px] bg-[#5E656C]">
+                    <div className="flex items-center 2xl:gap-[18px] xl:gap-[16px] lg:gap-[14px] md:gap-[12px] gap-2">
+                      <GiAlliedStar
+                        size={24}
+                        className="lg:min-w-[24px] lg:w-[24px] w-[20px] min-w-[20px]"
+                      />
 
-                      <h2 className="font-[400]">Call to Action</h2>
+                      <h2 className="font-[400] xl:text-[24px] lg:text-[22px] md:text-[20px] text-[18px]">
+                        Call to Action
+                      </h2>
                     </div>
                     <GradeBadge grade="F" />
                   </div>
                   <div className="md:px-[18px] px-4 py-[14px]">
-                    <p className="text-[#252A2E]">
+                    <p className="text-[#252A2E] xl:text-[18px] lg:text-[17px] md:text-[16px] text-[15px]">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                       Sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua.
