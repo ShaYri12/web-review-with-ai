bf8e9e0d3ce5103ed3d7efd4e9cc2f6a53c0f3f7
vikram kumar <vikramkumar8t8@gmail.com>
2025-01-20 20:47:44 +0500

    update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ import SocialLink from "./SocialLink";
 
 const Footer = ({ social = true }) => {
   return (
-    <footer className="px-4 lg:px-[24px] py-4 md:py-6 3xl:py-[32px] mt-[40px] lg:mt-[70px] w-full">
+    <footer className="px-4 lg:px-[24px] py-4 md:py-6 3xl:py-[32px] mt-[60px] lg:mt-[20px] w-full">
       <div className="3xl:max-w-[1600px] 2xl:max-w-[1280px] max-w-[1280px] mx-auto flex flex-wrap gap-4 justify-between items-center">
         <div
           className={`flex space-x-4 md:space-x-[24px] ${
